07d1bb4f201fc69f47f3d7bf49fc329071bf9877
Artem-Fox <121398407+Artem-Fox@users.noreply.github.com>
2025-07-27 18:21:05 +0300

    Added filters & pagination to RunViewSet & UserViewSet

--- app_run/views.py
+++ app_run/views.py
@@ -2,10 +2,13 @@ from rest_framework import viewsets, status
 from rest_framework.views import APIView
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework.filters import SearchFilter
+from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.pagination import PageNumberPagination
 from django.conf import settings
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404
+
+from django_filters.rest_framework import DjangoFilterBackend
 
 from .models import Run
 from .serializers import RunSerializer, UserSerializer
@@ -20,9 +23,17 @@ def company_details_view(request):
     })
 
 
+class SizePagination(PageNumberPagination):
+    page_size_query_param = "size"
+
+
 class RunViewSet(viewsets.ModelViewSet):
     queryset = Run.objects.all().select_related("athlete")
     serializer_class = RunSerializer
+    pagination_class = SizePagination
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filterset_fields = ["status", "athlete"]
+    ordering_fields = ["created_at"]
 
 
 class RunStartView(APIView):
@@ -58,10 +69,12 @@ class RunStopView(APIView):
 
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = User.objects.exclude(is_superuser=True)
+    queryset = User.objects.exclude(is_superuser=True).order_by("id")
     serializer_class = UserSerializer
-    filter_backends = [SearchFilter]
+    pagination_class = SizePagination
+    filter_backends = [SearchFilter, OrderingFilter]
     search_fields = ["first_name", "last_name"]
+    ordering_fields = ["date_joined"]
 
     def get_queryset(self):
         qs = self.queryset
