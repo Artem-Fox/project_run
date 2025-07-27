e1c109479af480d42e5bf979f1da7bc9e44bce04
Artem-Fox <121398407+Artem-Fox@users.noreply.github.com>
2025-07-27 13:55:29 +0300

    Added athlete_data to RunSerializer

--- app_run/serializers.py
+++ app_run/serializers.py
@@ -4,12 +4,6 @@ from django.contrib.auth.models import User
 from .models import Run
 
 
-class RunSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Run
-        fields = "__all__"
-
-
 class UserSerializer(serializers.ModelSerializer):
     type = serializers.SerializerMethodField()
 
@@ -22,3 +16,17 @@ class UserSerializer(serializers.ModelSerializer):
             return "coach"
 
         return "athlete"
+
+
+class AthleteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "username", "last_name", "first_name"]
+
+
+class RunSerializer(serializers.ModelSerializer):
+    athlete_data = AthleteSerializer(source="athlete", read_only=True)
+
+    class Meta:
+        model = Run
+        fields = "__all__"
