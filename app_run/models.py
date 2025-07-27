3f15b8712ebf17c16ff5e04c00772b488370af49
Artem-Fox <121398407+Artem-Fox@users.noreply.github.com>
2025-07-27 14:32:29 +0300

    Added status choices to Run model

--- app_run/models.py
+++ app_run/models.py
@@ -3,6 +3,13 @@ from django.db import models
 
 
 class Run(models.Model):
+    STATUS_CHOICES = [
+        ("init", "Забег инициализирован"),
+        ("in_progress", "Забег начат"),
+        ("finished", "Забег закончен")
+    ]
+
     athlete = models.ForeignKey(User, on_delete=models.CASCADE)
     comment = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=11, choices=STATUS_CHOICES, default="init")
