d48448ee1d9f8feca22d5db64b0d38fb8ce9e87f
Artem-Fox <121398407+Artem-Fox@users.noreply.github.com>
2025-10-04 15:58:56 +0300

    Рефакторинг: отредактированы админки

--- app_run/admin.py
+++ app_run/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Run, AthleteInfo, Challenge, Position, CollectibleItem
+from .models import Run, Challenge, CollectibleItem
 
 
 @admin.register(Run)
@@ -8,30 +8,22 @@ class RunAdmin(admin.ModelAdmin):
     list_display = ["id", "created_at", "athlete", "status"]
     list_display_links = ["id", "created_at"]
     list_filter = ["status"]
-    list_per_page = 10
-    search_fields = ["athlete__username"]
-
-
-@admin.register(AthleteInfo)
-class AthleteInfoAdmin(admin.ModelAdmin):
-    list_display = ["athlete", "goals", "weight"]
-    list_per_page = 10
+    list_per_page = 20
     search_fields = ["athlete__username"]
 
 
 @admin.register(Challenge)
 class ChallengeAdmin(admin.ModelAdmin):
-    list_display = ["full_name", "athlete"]
-    list_per_page = 10
+    list_display = ["id", "full_name", "athlete"]
+    list_display_links = ["id", "full_name"]
+    list_filter = ["full_name", "athlete"]
+    list_per_page = 20
     search_fields = ["full_name", "athlete__username"]
-
-
-admin.site.register(Position)
 
 
 @admin.register(CollectibleItem)
 class CollectibleItemAdmin(admin.ModelAdmin):
     list_display = ["uid", "name"]
     list_display_links = ["uid", "name"]
-    list_per_page = 10
+    list_per_page = 20
     search_fields = ["uid", "name"]
